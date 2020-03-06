0d0b4a43231e69f44b5350132e44454d141a7cc8
NaSinSa <andy.k.oh@hotmail.com>
2020-03-06 14:48:33 -0500

    create comments table and comments seeds file

---
+++ db/schema/03_comments.sql
@@ -0,0 +1,10 @@
+-- Drop and recreate Comments table (Example)
+
+DROP TABLE IF EXISTS comments CASCADE;
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  content TEXT,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  hook_id INTEGER REFERENCES hooks(id) ON DELETE CASCADE
+);
---
+++ db/seeds/03_comments.sql
@@ -0,0 +1,3 @@
+-- Users table seeds here (Example)
+INSERT INTO comments (content, user_id, hook_id)
+VALUES ('good!', 1, 1)
