4172556ce5eaa7adb1fb65aed5a325bea544165f
NaSinSa <andy.k.oh@hotmail.com>
2020-03-06 14:04:30 -0500

    add hooks table which posts resources

---
+++ db/schema/02_posts.sql
@@ -0,0 +1,15 @@
+-- Drop and recreate Hooks table (Example)
+
+DROP TABLE IF EXISTS hooks CASCADE;
+
+CREATE TABLE hooks (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  content TEXT,
+  comment_id INTEGER REFERENCES comments(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  like_id INTEGER REFERENCES likes(id) ON DELETE CASCADE,
+  rating INTEGER,
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+);
