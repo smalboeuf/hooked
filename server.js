58c5f238455346865e1028ecc967de3c506cb896
Lee <notayogilee@gmail.com>
2020-03-06 20:55:52 +0000

    routes

--- server.js
+++ server.js
@@ -50,6 +50,19 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/login", (req,res) => {
+  res.render("login");
+});
+
+// app.post("/login", (req,res) => {
+
+//   res.render("index");
+// });
+
+app.get("/register", (req,res) => {
+  res.render("register");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
