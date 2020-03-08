eaed4a536af70140311e87eeb428985cebbf0598
Lee <notayogilee@gmail.com>
2020-03-08 21:21:34 +0000

    cookies are installed, but need work

--- routes/login.js
+++ routes/login.js
@@ -1,23 +1,41 @@
 const express = require('express');
 const router = express.Router();
 const cookieSession = require('cookie-session');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
+router.use(cookieSession({
+  name: 'session',
+  keys: ['hummus', 'pen', 'working'],
+  maxAge: 24 * 60 * 60 * 1000
+}));
+
+let users = { id: 1, name: "Alice", email: "alice@email.ca", password: "password" };
 
 module.exports = () => {
-  router.use(cookieSession({
-    name: 'session',
-    keys: ['hummus', 'pen', 'working'],
-    maxAge: 24 * 60 * 60 * 1000
-  }));
 
   router.get("/login", (req, res) => {
     res.render("login");
-  })
+  });
 
-  router.get("/login/id", (req, res) => {
-    req.session.user_id = req.params.id;
-    res.redirect("/");
+  // router.get("/login/:id", (req, res) => {
+  //   if (req.body.email === users.email) {
+  //     user = users;
+  //   }
+  //   req.session.user_id = req.params.id;
+  //   res.redirect("/urls");
+  // });
+
+  router.post("/login", (req, res) => {
+    if (req.body.email === users.email) {
+      user = users;
+      req.session['user_id'] = user.id;
+    }
+    res.render("index");
+  });
+
+  router.post("/logout", (req, res) => {
+    res.session = null;
+    res.redirect("/login");
   });
 
   return router;
