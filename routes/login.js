af3068d9fe8a7252f65f70d122c891df32355393
Lee <notayogilee@gmail.com>
2020-03-08 18:38:16 +0000

    login and register routes working in proper place

--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const cookieSession = require('cookie-session');
+// const bcrypt = require('bcrypt');
 
 
 module.exports = () => {
+  router.use(cookieSession({
+    name: 'session',
+    keys: ['hummus', 'pen', 'working'],
+    maxAge: 24 * 60 * 60 * 1000
+  }));
 
   router.get("/login", (req, res) => {
     res.render("login");
-  });
+  })
 
-  router.post("login", (req, res) => {
-
-    res.render("index");
+  router.get("/login/id", (req, res) => {
+    req.session.user_id = req.params.id;
+    res.redirect("/");
   });
 
   return router;
--- routes/register.js
+++ routes/register.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 
+
 module.exports = () => {
 
   router.get("/register", (req, res) => {
