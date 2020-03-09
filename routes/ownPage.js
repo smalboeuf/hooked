5fc613708ee848c1e1fc5b28c95ab405547d3413
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-03-09 14:57:38 +0000

    Fixed template

---
+++ routes/ownPage.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const cookieSession = require('cookie-session');
+const bcrypt = require('bcrypt');
+
+router.use(cookieSession({
+  name: 'session',
+  keys: ['hummus', 'pen', 'working'],
+  maxAge: 24 * 60 * 60 * 1000
+}));
+
+let users = { id: 1, name: "Alice", email: "alice@email.ca", password: "password" };
+
+
+module.exports = () => {
+
+  router.get("/ownPage", (req, res) => {
+    const templateVars = {id: req.session.user_id};
+    res.render("ownPage", templateVars);
+  });
+
+  return router;
+};
