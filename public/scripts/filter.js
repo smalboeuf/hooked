c18bf43a6e0f3d2197b3af3600f93fdec88aa3b8
NaSinSa <andy.k.oh@hotmail.com>
2020-03-11 09:20:09 -0400

    filter works

---
+++ public/scripts/filter.js
@@ -0,0 +1,26 @@
+
+
+$(document).ready(function() {
+
+  $('select.category-filter').change(function(){
+
+    let selectedCategory = $(this).children("option:selected").val();
+
+    $('#hooksContainer').children('div').each(function () {
+      if ($(this).attr('name') !== selectedCategory) {
+            $(this).slideUp();
+          } else {
+            $(this).slideDown();
+          }
+  });
+
+    // for (const ele of $('#hooksContainer').childNodes)
+    //   if (ele.attribute.name === selectedCategory) {
+    //     $(this).addClass('display', 'hidden');
+    //   } else {
+    //     $(this).removeClass('display');
+    //   }
+
+  });
+
+});
